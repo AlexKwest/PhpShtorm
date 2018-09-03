16b7593cbf5bec92e58584eac12fc709c69b8d05
AlexKwest <lex.kaname@gmail.com>
2018-09-03 21:01:34 +0000

    add class BirthDate => BirthDate.php

---
+++ src/Dates/BirthDate.php
@@ -0,0 +1,18 @@
+<?php
+namespace Nesterenko\Dates;
+/**
+ * 
+ */
+class BirthDate 
+{
+	
+	private $birthDate;
+	private $todayDate;
+
+	public funnction __Constract($birthDate)
+	{
+		
+	}
+}
+
+?>
